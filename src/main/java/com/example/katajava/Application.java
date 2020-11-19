53a22e0225a4da00f043306ce6a37df8f9b2270d
cmlozanos <carlosmlozanos@gmail.com>
2020-11-19 17:26:47 +0100

    init commit

---
+++ src/main/java/com/example/katajava/Application.java
@@ -0,0 +1,8 @@
+package com.example.katajava;
+
+public class Application {
+
+	public static void main(final String[] args) {
+		System.out.println("Hello Kata");
+	}
+}
---
+++ src/test/java/com/example/katajava/ApplicationTest.java
@@ -0,0 +1,11 @@
+package com.example.katajava;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class ApplicationTest {
+	@Test
+	void testcase1() throws Exception {
+		Assertions.assertTrue(true);
+	}
+}
